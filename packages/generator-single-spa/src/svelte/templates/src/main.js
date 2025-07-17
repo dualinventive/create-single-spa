f9d2fcdb83c7485d83c5c67dcb68632c86236c5c
Maarten Manders <maarten.manders@dualinventive.com>
2025-07-17 14:54:53 +0200

    Add required mount and unmount options.

--- packages/generator-single-spa/src/svelte/templates/src/main.js
+++ packages/generator-single-spa/src/svelte/templates/src/main.js
@@ -1,8 +1,11 @@
 import singleSpaSvelte from "single-spa-svelte";
+import { mount as originalMount, unmount as originalUnmount } from "svelte";
 import App from "./App.svelte";
 
 const svelteLifecycles = singleSpaSvelte({
   component: App,
+  mount: originalMount,
+  unmount: originalUnmount,
 });
 
 export const { bootstrap, mount, unmount } = svelteLifecycles;
